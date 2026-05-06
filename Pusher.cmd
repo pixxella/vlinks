f030bf8f71dbb84efc7880616049896d53eb774e
pixxella <noplayersonmyway@gmail.com>
2026-05-06 21:34:46 +0300

    Auto-commit: Wed 05/06/2026 21:34:46.56

--- Pusher.cmd
+++ Pusher.cmd
@@ -1,18 +1,33 @@
 @echo off
+setlocal
 
 cd /d "%~dp0"
-git add readme.md
-echo added readme
-git add index.html
-echo added index
-git add OldestIndex.html
-echo added oldest index
-git add Pusher.cmd
-echo added pusher
-git commit -m "%random%"
-echo committed changes
-git push origin master
-echo pushed changeses
-echo done wait 30 sec
+
+:parse
+if "%~1"=="" goto :default_commit
+if "%~1"=="-c" goto :set_commit
+if "%~1"=="--commit" goto :set_commit
+shift
+goto :parse
+
+:set_commit
+set "COMMIT_MSG=%~2"
+shift
+shift
+goto :parse
+
+:default_commit
+if "%COMMIT_MSG%"=="" set "COMMIT_MSG=Auto-commit: %date% %time%"
+
+echo Adding files...
+git add . || (echo Failed to add files & exit /b 1)
+
+echo Committing with message: "%COMMIT_MSG%"
+git commit -m "%COMMIT_MSG%" || (echo Nothing to commit & goto :done)
+
+echo Pushing...
+git push origin master || (echo Failed to push & exit /b 1)
+
+:done
+echo Done! Waiting 30 seconds...
 timeout /t 30
-echo DONE
